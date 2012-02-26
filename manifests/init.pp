fb69e1ef6b73d275b73721226c0411e7daca1214
Stefano Zanella <stefano.zanella@derecom.it>
2012-02-26 03:32:39 +0100

    Imported project in Eclipse + Geppetto; altered metadata.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,20 @@
+# = Class: rsync
+#
+# This class manages an rsync server.
+#
+# == Parameters:
+#
+# == Actions:
+#
+# == Requires:
+#
+# == Sample Usage:
+#
+# == Author:
+#   Stefano Zanella <stefano@dontwakethecat.net> @stefano_zanella
+#
+# == License:
+#   Apache Version 2
+#
 class rsync {
 }
